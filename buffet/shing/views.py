00bde164600a83a42ee7205bc62dcdb191a7cc84
Xinyi Ding <dxywill@gmail.com>
2016-05-27 00:20:07 -0500

    Add templates

--- buffet/shing/views.py
+++ buffet/shing/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+import tushare as ts
 
 # Create your views here.
 def index(request):
     if request.method == 'GET':
-        return HttpResponse("Hello Buffet")
+    	df = ts.get_stock_basics()
+    	print df.size
+    	return render(request, 'index.html')
+        #return HttpResponse("Hello Buffet")
     else:
         print ("get post")
         return JsonResponse({'foo':'bar'})
